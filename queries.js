dae3f645d26275fdb8c0174d823ba0b6f567621e
Arthurchege <arthurchege3@gmail.com>
2025-10-06 09:29:15 +0300

    Wk_1-Assignment_MongoDb

---
+++ queries.js
@@ -0,0 +1,142 @@
+// Task 1 
+//(A)
+use plp_bookstore
+
+//(B)
+db.createCollection("books")
+
+// Task 2 
+//(A)
+db.books.insertMany([{
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Fiction',
+    published_year: 1960,
+    price: 12.99,
+    in_stock: true,
+    pages: 336,
+    publisher: 'J. B. Lippincott & Co.'
+  },
+  {
+    title: '1984',
+    author: 'George Orwell',
+    genre: 'Dystopian',
+    published_year: 1949,
+    price: 10.99,
+    in_stock: true,
+    pages: 328,
+    publisher: 'Secker & Warburg'
+  },
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    genre: 'Fiction',
+    published_year: 1925,
+    price: 9.99,
+    in_stock: true,
+    pages: 180,
+    publisher: 'Charles Scribner\'s Sons'
+  },
+  {
+    title: 'Brave New World',
+    author: 'Aldous Huxley',
+    genre: 'Dystopian',
+    published_year: 1932,
+    price: 11.50,
+    in_stock: false,
+    pages: 311,
+    publisher: 'Chatto & Windus'
+  },
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    genre: 'Fantasy',
+    published_year: 1937,
+    price: 14.99,
+    in_stock: true,
+    pages: 310,
+    publisher: 'George Allen & Unwin'
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    genre: 'Fiction',
+    published_year: 1951,
+    price: 8.99,
+    in_stock: true,
+    pages: 224,
+    publisher: 'Little, Brown and Company'
+  },
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    genre: 'Romance',
+    published_year: 1813,
+    price: 7.99,
+    in_stock: true,
+    pages: 432,
+    publisher: 'T. Egerton, Whitehall'
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    genre: 'Fantasy',
+    published_year: 1954,
+    price: 19.99,
+    in_stock: true,
+    pages: 1178,
+    publisher: 'Allen & Unwin'
+  },
+  {
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    genre: 'Political Satire',
+    published_year: 1945,
+    price: 8.50,
+    in_stock: false,
+    pages: 112,
+    publisher: 'Secker & Warburg'
+  },
+  {
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    genre: 'Fiction',
+    published_year: 1988,
+    price: 10.99,
+    in_stock: true,
+    pages: 197,
+    publisher: 'HarperOne'
+  },])
+
+//(B)
+db.books.find().sort({ genre: "Fiction" })
+
+//(C)
+db.books.find({published_year:{$gte:1950}})
+
+//(D)
+db.books.find({ author: "George Orwell" })
+
+//(E)
+db.books.find({ author: "George Orwell" })
+
+//(F)
+db.books.deleteOne({author:"J.D. Salinger"})
+
+//Task 3
+//(A)
+db.books.find({ $and: [ { in_stock: true }, { published_year: { $gte: 2010 } } ] })
+
+
+//(B)
+db.books.find({}, { title: 1, author: 1, price:1 })
+
+//(C)
+db.books.find().sort({ price: 1 })
+db.books.find().sort({ price: -1 })
+
+//(D) 
+db.books.find().skip(0).limit(5)
+
+// Task 4
+//(A)
